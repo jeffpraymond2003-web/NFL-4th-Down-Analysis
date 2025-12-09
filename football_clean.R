49b52af94ff01b6fd7c30ccf5df62733c623c44c
marten-shears <marten.shears@drake.edu>
2025-12-09 16:30:46 -0600

    Add files via upload
    Signed-off-by: marten-shears <marten.shears@drake.edu>

--- football_clean.R
+++ football_clean.R
@@ -26,10 +26,20 @@ fourth_sub <- fourth_clean %>% select(c(away_coach,away_score,away_team,away_tim
                                         total_home_raw_yac_epa,total_home_raw_yac_wpa,total_home_rush_epa,total_home_rush_wpa,total_home_score,total_line,vegas_home_wp,
                                         vegas_wp,week,wind,wp,yardline_100,ydstogo))
 
+fourth_sub <- fourth_clean %>% select(c(away_coach,away_score,away_team,away_timeouts_remaining,away_wp,cp,def_wp,defteam,defteam_score,defteam_timeouts_remaining,
+                                        div_game,down,drive,drive_game_clock_start,drive_quarter_start,drive_real_start_time,drive_start_transition,
+                                        ep,fg_prob,fourth_down_converted,game_date,game_half,game_stadium,goal_to_go,
+                                        home_coach,home_score,home_team,home_timeouts_remaining,home_wp,lateral_reception,lateral_rush,location,no_huddle,no_score_prob,
+                                        opp_fg_prob,opp_safety_prob,opp_td_prob,pass_attempt,pass_length,pass_location,pass_oe,passer,
+                                        play_type,posteam,posteam_score,posteam_timeouts_remaining,posteam_type,qb_dropback,qtr,quarter_seconds_remaining,
+                                        receiver,roof,run_gap,run_location,rush_attempt,rusher,score_differential,season,season_type,series,shotgun,
+                                        side_of_field,spread_line,stadium,start_time,surface,temp,total,total_away_epa,total_away_score,
+                                        total_home_epa,total_home_score,total_line,vegas_home_wp,vegas_wp,week,wind,wp,yardline_100,ydstogo))
+
 rm(fourth)
 rm(fourth_clean)
 
-# CLEAN UP THIS SECTION + ADD DOCUMENTATION
+# CLEAN UP THIS SECTION + ADD DOCUMENTATION - COMBINE HOME/AWAY FOR POSTEAM
 
 # fourth_down_converted - factor for simplicity
 # cp - introduce complete separation
@@ -59,8 +69,8 @@ fourth_sub <- fourth_sub %>% mutate(fourth_down_converted = factor(fourth_down_c
                                     ,start_time = as.numeric(hour(mdy_hms(start_time)))
                                     ,temp = if_else(is.na(temp) == TRUE, 72, temp)
                                     ,wind = if_else(is.na(wind) == TRUE, 0, wind)
-                                    ,passer = if_else(play_type == 'pass',if_else(is.na(passer) == TRUE, 'No Passer', passer),'Rush Attempt')
-                                    ,rusher = if_else(is.na(rusher) == TRUE, 'No Rusher', rusher)
+                                    ,total_posteam_epa = if_else(posteam_type == 'home', total_home_epa, total_away_epa)
+                                    ,total_defteam_epa = if_else(posteam_type == 'home', total_away_epa, total_home_epa)
                                     ,playcall = if_else(play_type == 'pass',paste0(play_type,if_else(is.na(pass_length) == TRUE,'',paste0(' ',pass_length)),if_else(is.na(pass_location) == TRUE,'',paste0(' ',pass_location))),paste0(play_type,if_else(is.na(run_location) == TRUE,'',paste0(' ',run_location)),if_else(is.na(run_gap) == TRUE,'',paste0(' ',run_gap))))
                                     ,target_player = if_else(play_type == 'pass', if_else(is.na(receiver) == TRUE, passer, receiver), if_else(is.na(rusher) == TRUE, passer, rusher))) %>% 
   rename(game_year = game_date)
---
+++ football_forest.R
@@ -0,0 +1,65 @@
+library(tidyverse)
+library(tidymodels)
+library(randomForest)
+library(pROC)
+library(rpart)
+library(rpart.plot)
+library(reshape2)
+
+RNGkind(sample.kind = "default")
+set.seed(4561)
+train.idx <- sample(x = 1:nrow(fourth_final), size = floor(.7*nrow(fourth_final)))
+
+train.df <- fourth_final[train.idx,]
+train.df <- subset(train.df)
+test.df <- fourth_final[-train.idx,]
+
+rf_model <- rand_forest(mtry = tune(),
+                        trees = 1000) %>% 
+  set_mode("classification") %>% 
+  set_engine("randomForest")
+
+# (step 2) Create a recipe
+rf_rec <- recipe(fourth_down_converted ~ ., data = train.df) #use training data set
+
+# (step 3) Create the workflow
+rf_wf <- workflow() %>%
+  add_model(rf_model) %>% #from step 1
+  add_recipe(rf_rec) #from step 2
+
+# (step 4) Create folds for cross validation (see previous illustration)
+set.seed(172172)
+folds <- vfold_cv(train.df, v = 5)
+
+# (step 5) tune random forest
+rf_tuned <- tune_grid(
+  rf_wf, 
+  resamples = folds, 
+  grid = tibble(mtry = c(1:5,10,15,20)), 
+  metrics = metric_set(roc_auc)
+)
+
+# (step 6) extract AUC and/or OOB error estimates
+rf_results <- rf_tuned %>%
+  collect_metrics()
+
+ggplot(data = rf_results) +
+  geom_line(aes(x = mtry, y = mean)) +
+  labs(x = "m (mtry) value", y = "Area Under the Curve (AUC)")+
+  theme_bw() +
+  scale_x_continuous(breaks = c(1:20))
+
+best_params <- select_best(rf_tuned, metric ="roc_auc")
+final_forest<- randomForest(fourth_down_converted ~ .,
+                            data = train.df,
+                            ntree = 1000, #fix B at 1000!
+                            mtry = best_params %>% pull(mtry),#chosen from the tuning
+                            importance = TRUE) #keep importance
+
+pi_hat_forest<- predict(final_forest, test.df, type = "prob")[, "Yes"]
+rocCurve <- roc(response = test.df$fourth_down_converted,#supply truth
+                predictor = pi_hat_forest,#supply predicted PROBABILITIES)
+                levels = c("No", "Yes"))
+plot(rocCurve,print.auc = TRUE, print.thres=TRUE)
+
+varImpPlot(final_forest,type=1)
